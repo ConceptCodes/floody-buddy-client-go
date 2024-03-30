7ab83e262680d1b020bd49f20ee4de567b6aa866
concept <conceptcodes@gmail.com>
2024-03-30 15:18:23 -0400

    Refactor main.go to use context and add signal handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,34 @@
 package client
 
 import (
+	"context"
+	"floody-buddy/config"
 	"floody-buddy/internal/spammer"
 	"floody-buddy/pkg/logger"
-	"floody-buddy/config"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func Run() {
+func Start() {
 	log := logger.New()
 
 	workers := config.AppConfig.Workers
-	
+
 	log.Info().Msgf("Initializing %d Workers", workers)
+	log.Info().Msgf("Targeting the Following URLs: %s", config.AppConfig.Domains)
+
+	ctx, cancel := context.WithCancel(context.Background())
 
 	for i := 0; i < workers; i++ {
-		go func() { spammer.Flood() }()
+		go func() { spammer.Flood(ctx, cancel) }()
 	}
+
+	// Wait for a signal to shutdown.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+
+	// Cancel the context, which will stop the Flood operations.
+	cancel()
 }

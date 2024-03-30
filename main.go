16a9e385fc810f30c79f9c454c885beb02d18f5a
concept <conceptcodes@gmail.com>
2024-03-30 15:18:31 -0400

    Update main.go to use version constant and rename Run to Start

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"floody-buddy/cmd"
+	client "floody-buddy/cmd"
+	"floody-buddy/internal/constants"
 	"floody-buddy/pkg/logger"
 )
 
 func main() {
 	log := logger.New()
-	log.Info().Msg("Starting Floody Buddy")
+	log.Info().Msgf("Starting Floody Buddy v%s", constants.VERSION)
 
-	client.Run()
+	client.Start()
 
 	select {}
 }
